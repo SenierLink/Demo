9833615754867d778b963f6c200a4a850ef2d7ab
SenierLink <1325181337@qq.com>
2019-10-19 20:14:50 +0800

    完成ajax——_json，php从mysql中取值以JSON格式传递数据

--- StudentManager/php/fetch_data.php
+++ StudentManager/php/fetch_data.php
@@ -1,24 +1,27 @@
 <?php
 
-    // header();
+    header('Content-Type:application/json; charset=utf-8');
     $dbusr = "link";
     $servername = "localhost";
     $dbpasswd = "1007";
     $dbname = "demo";
     
+    
     $conn = new mysqli($servername, $dbusr, $dbpasswd, $dbname);
     if ($conn->connect_error) {
         die("连接失败: " . $conn->connect_error);
     } 
-    echo "连接成功";
 
 
     $sql_fetch_data = "select * from studentmanager";
     $result = $conn->query($sql_fetch_data);
     if($result->num_rows > 0){
-        while($temp = $result->fetch_assoc()){
-            var_dump($temp);
+        $arr = array();
+        while($row = $result->fetch_assoc()){
+            // var_dump($temp);
+            $arr[count($arr)] = $row;
         }
+        echo json_encode($arr);
     }
 
 
--- StudentManager/scripts/main.js
+++ StudentManager/scripts/main.js
@@ -6,12 +6,12 @@ window.addEventListener("load", show_data)
  * @returns {Array} 把每行数据作为一个元素，数据{object}，具有属性id，name，birthday，sex
  */
 function fetch_data() {
-    //同步
+    // 同步
     // var xhr = new XMLHttpRequest()
-    // xhr.open(get, "../php/fetch_data.php", flase)
+    // xhr.open("get", "../php/fetch_data.php", "flase")
     // xhr.send
     // if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-    //     alert(xhr.responseText);
+    //     return JSON.parse(xhr.responseText)
     // } else {
     //     alert("Request was unsuccessful:" + xhr.status);
     // }
@@ -21,20 +21,44 @@ function fetch_data() {
         if (xhr.readyState == 4) {
             if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
                 console.log(xhr.responseText);
+                doDom(JSON.parse(xhr.responseText))
+                return JSON.parse(xhr.responseText)
+                // console.log(xhr.responseType)
+                // for(let i = 0; i<xhr.responseText.length; i++){
+                //     xhr.responseText[i] = JSON.parse(xhr.responseText[i])
+                // }
+
             } else {
                 alert("Request was unsuccessful:" + xhr.status);
             }
         }
     }
-
+    
     xhr.open("GET", "./php/fetch_data.php", true)
     xhr.send()
 }
 
+
+function doDom(data){
+    console.log(data)
+    for(let i = 0; i<data.length; i++){
+        var temp = data[i]
+        
+        var div = document.createElement("div")
+        var p = document.createElement("p")
+        p.innerText = temp.id + temp.name + temp.birthday + temp.sex
+        div.appendChild(p)
+
+        document.body.appendChild(div)
+    }
+}
 
 /**
  * 以js操作dom显示data
  */
 function show_data() {
     fetch_data()
+    // var data = fetch_data()
+    // console.log(data) :undifinde
+    // 鬼知道xhr里面的return怎么返回的。反正在xhr的里面写handleonsuccess(本次用的doDom)就不会有问题。估计是console.log(data)的时候，还没return。鬼知道原因，差半天没查到。
 }
